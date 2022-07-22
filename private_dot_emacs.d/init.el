e42fe599616be685502bc279d32ff88e054c0623
Chad McAuley <inertquartet@github.com>
2022-07-22 15:56:26 -0500

    Commiting current Emacs config for posterity before I declare init.el bankruptcy........

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,31 @@
+(require 'package)
+
+;; Only use HTTPS if GnuTLS is present
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  ; ;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.ist-
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; Bootstrap use-package if it is not installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
+;; (server-start)
